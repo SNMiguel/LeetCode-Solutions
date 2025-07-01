0aea14ba16f13725bc05c87f35dc11cd31387ea4
Miguel Shema Ngabonziza <shemamiguel2023@gmail.com>
2025-07-01 11:39:21 -0400

    COMMIT33

---
+++ LeetCode-Solution-N3330.py
@@ -0,0 +1,29 @@
+# LeetCode problem N3330: Count Possible Strings
+# Difficulty: Easy
+
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        
+        #  Step 1: Initialize a variable to count the number of possible strings and set it to 1 for the original word
+        result = 1
+        
+        # Step 2: Iterate through the word starting from the second character
+        for i in range(1, len(word)):
+            
+            # Step 3: If the current character is the same as the previous one, increment the count
+            if word[i] == word[i-1]:
+                result += 1
+                
+        # Step 4: Return the total count of possible words
+        return result
+    
+word = "abbcccc"
+word1 = "aaaa"
+word2 = "abcd"
+solution = Solution()
+print(solution.possibleStringCount(word))   # Output: 5
+print(solution.possibleStringCount(word1))  # Output: 4
+print(solution.possibleStringCount(word2))  # Output: 1
+
+# Time Complexity: O(n), where n is the length of the word
+# Space Complexity: O(1), as we are using a constant amount of space for the
