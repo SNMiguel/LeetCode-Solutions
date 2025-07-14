aa821d20562320106f7f9f9f16c75c3bd4b12bea
Miguel Shema Ngabonziza <shemamiguel2023@gmail.com>
2025-07-14 01:39:45 -0400

    commit41

---
+++ LeetCode-Solution-N136.py
@@ -0,0 +1,16 @@
+# LeetCode problem N136: Single Number
+# Difficulty: Easy
+
+from collections import Counter
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        # Step 1: Count the occurrences of each number in the list
+        dic_nums = Counter(nums)
+        
+        # Step 2: Iterate through the dictionary to find the number that occurs only once
+        for key, value in  dic_nums.items():
+            if value == 1:
+                return key
+                break
