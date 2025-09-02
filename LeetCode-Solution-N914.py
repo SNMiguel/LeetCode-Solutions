3460440f0ca08fcecb6c7cc128f01a44ad4acb1c
Miguel Shema Ngabonziza <shemamiguel2023@gmail.com>
2025-09-02 04:01:53 -0400

    commit 09/02/2025 p2

--- LeetCode-Solution-N914.py
+++ LeetCode-Solution-N914.py
@@ -14,3 +14,11 @@ class Solution:
         
         # Step 3: Compute the GCD of all counts and check if it's at least 2 or more
         return False if reduce(gcd, deck_c) == 1 else True
+    
+deck = [1,2,3,4,4,3,2,1]
+deck1 = [1,1,1,2,2,2,3,3]
+print(Solution().hasGroupsSizeX(deck))  # Expected output: True
+print(Solution().hasGroupsSizeX(deck1)) # Expected output: False
+    
+# Time Complexity: O(N) where N is the number of cards in the deck
+# Space Complexity: O(N) in the worst case where all cards are unique
